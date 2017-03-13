9327388e71ec73ad4aeec3e3f39fceedc48c1368
Alex Richardson <alex@alexcr.com>
2017-03-12 20:41:29 -0400

    test

--- src/routes/stats/stats.controller.js
+++ src/routes/stats/stats.controller.js
@@ -14,7 +14,7 @@ export async function players(ctx) {
   const db = ctx.mongo.db('sonic').collection('playerStats');
   let playersInDb = await db.find().toArray();
 
-  if (playersInDb.length === 0) {
+  if (!playersInDb || playersInDb.length === 0) {
     const json = await getPlayerStats();
     db.insert(json.data);
     playersInDb = json.data;
